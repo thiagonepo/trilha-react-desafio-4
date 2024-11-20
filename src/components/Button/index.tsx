a98a4f33534bec282eb831caed94fe3b38260f14
Thiago Nepomuceno <90644531+thiagonepo@users.noreply.github.com>
2024-11-19 23:33:20 -0300

    Refactor index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
 const Button = ({ title, onClick, disabled }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+  return <ButtonContainer disabled={disabled} onClick={onClick}>{title}</ButtonContainer>;
 };
 
 export default Button;
