fd378e083c1655b306fd2ff292764eb4cf68bf09
Thiago Nepomuceno <90644531+thiagonepo@users.noreply.github.com>
2024-11-19 23:26:10 -0300

    Update index.tsx

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
+const Button = ({ title, onClick, disabled }: IButtonProps) => {
   return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
 };
 
