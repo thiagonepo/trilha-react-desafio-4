b61d683f14accd17458488f88067b7aa89cd6669
Thiago Nepomuceno <90644531+thiagonepo@users.noreply.github.com>
2024-11-19 23:28:27 -0300

    Update styles.ts

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -11,7 +11,11 @@ export const ButtonContainer = styled.button`
     border-radius: 21px;
 
     &:hover {
-        opacity: 0.6;
+        opacity: 0.8;
         cursor:pointer;
+    }
+    &:disabled {
+        opacity: 0.8;
+        cursor:no-drop;
     }
 `
